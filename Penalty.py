2c3b323c265bd1059cc9749343352b4047b161de
tonygarhager <tony.garhager@gmail.com>
2024-12-24 01:46:47 -0800

    AppliedPenaly

---
+++ Penalty.py
@@ -0,0 +1,28 @@
+
+class PenaltyType:
+    Unknown = 0
+    TagMismatch = 1
+    MemoryTagsDeleted = 2
+    TargetSegmentMismatch = 3
+    FilterPenalty = 4
+    ProviderPenalty = 5
+    MultipleTranslations = 6
+    AutoLocalization = 7
+    TextReplacement = 8
+    Alignment = 9
+    CharacterWidthDifference = 10
+    NotTranslated = 11
+    Draft = 12
+    Translated = 13
+    RejectedTranslation = 14
+    ApprovedTranslation = 15
+    RejectedSignOff = 16
+    ApprovedSignOff = 17
+
+class AppliedPenalty:
+    def __init__(self, pt: PenaltyType, malus:int):
+        self.type = pt
+        self.malus = malus
+        self.filter_name = None
+
+    
